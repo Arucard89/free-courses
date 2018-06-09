134e82f052f0d141dba532c8b351be8e1eb900c2
Kirill <revenkov-k@mail.ru>
2018-06-09 08:48:29 +0300

    escape from MAth.pow

--- task1.js
+++ task1.js
@@ -3,16 +3,30 @@
 'use strict';
 var n = + process.argv[2];
 var e = + process.argv[3];
-process.stdout.write(SumOfPower(n, e).toString());
+process.stdout.write(sumOfPower(n, e).toString());
 
-function SumOfPower(n, e) {
+function sumOfPower(n, e) {
 //return e > 1 ? (Math.pow(n,e) + SumOfPower(n,e - 1)) : 1;
 var res;
 if (n > 1) {
-res = Math.pow(n, e) + SumOfPower(n - 1, e);
+res = power(n, e) + sumOfPower(n - 1, e);
 }
 else {
 res = 1;
 };
 return res;
-}
+};
+
+function power(n,e){
+	var res = 1;
+	var p;
+	e > 0 ? p = e: p = ~e + 1; //abs
+	for (var i = 0;i < p; i++) {
+		res *= n;
+	};
+	
+	if (e < 0){
+	    res= 1 / res;
+	};
+	return res;
+};
